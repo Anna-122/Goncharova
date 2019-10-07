965e0fa22aefaec8f80475659903f2fa0d92f626
serg <serg@email.com>
2019-10-07 21:13:49 +0300

    init15

---
+++ src/lesson15/ResourceBundleDemo.java
@@ -0,0 +1,30 @@
+package lesson15;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Student on 07.10.2019.
+ */
+public class ResourceBundleDemo {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        printInfo(Locale.ROOT);
+
+        printInfo( new Locale("en", "US"));
+        printInfo( new Locale("uk", "UA"));
+    }
+
+    private static void printInfo(Locale locale)
+            throws UnsupportedEncodingException {
+        ResourceBundle rb = ResourceBundle.getBundle("topic", locale);
+        for (String key : rb.keySet()) {
+            String value = rb.getString(key);
+            value = new String(value.getBytes("ISO-8859-1"), "UTF-8");
+            System.out.println(value);
+        }
+        System.out.println();
+    }
+}
+
+
